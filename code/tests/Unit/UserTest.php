dc32c5c59f8061403733deca221fc98ae2d0f297
ahmdkamal <aka.3awd@gmail.com>
2021-02-18 13:17:11 +0200

    add test cases

--- code/tests/Unit/UserTest.php
+++ code/tests/Unit/UserTest.php
@@ -11,6 +11,7 @@ use Tests\TestCase;
 class UserTest extends TestCase
 {
     protected $userService;
+
     public function setUp(): void
     {
         parent::setUp();
@@ -24,11 +25,79 @@ class UserTest extends TestCase
         ]);
     }
 
-    public function testGetAllProviderX()
+    public function testGetAllProviders()
     {
         $request = new Request();
         $response = $this->userService->getAllUsers($request);
-        dd($response);
-        $this->assertCount(4, $response);
+        $this->assertCount(5, $response);
+    }
+
+    public function testFilterByProviderX()
+    {
+        $request = new Request(['provider' => 'DataProviderX']);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(3, $response);
+    }
+
+    public function testFilterByProviderY()
+    {
+        $request = new Request(['provider' => 'DataProviderY']);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(2, $response);
+    }
+
+    public function testFilterByRefundedStatusCode()
+    {
+        $request = new Request(['statusCode' => 'refunded']);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(1, $response);
+    }
+
+    public function testFilterByDeclineStatusCode()
+    {
+        $request = new Request(['statusCode' => 'decline']);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(2, $response);
+    }
+
+    public function testFilterByAuthorizedStatusCode()
+    {
+        $request = new Request(['statusCode' => 'authorised']);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(2, $response);
+    }
+
+    public function testFilterByMinBalance()
+    {
+        $request = new Request(['balanceMin' => 1000]);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(2, $response);
+    }
+
+    public function testFilterByMaxBalance()
+    {
+        $request = new Request(['balanceMax' => 100]);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(1, $response);
+    }
+
+    public function testFilterByCurrency()
+    {
+        $request = new Request(['currency' => 'AED']);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(1, $response);
+    }
+
+    public function testFilterByAllFilters()
+    {
+        $request = new Request([
+            'currency' => 'USD',
+            'provider' => 'DataProviderX',
+            'statusCode' => 'decline',
+            'balanceMin' => 0,
+            'balanceMax' => 100
+        ]);
+        $response = $this->userService->getAllUsers($request);
+        $this->assertCount(1, $response);
     }
 }
